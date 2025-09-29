7f38b52b20171b9c9b8612cf58d767781f92e178
dladiera <dladeiratx@gmail.com>
2025-09-29 22:07:04 +0200

    feat(dashboards)!: changed boilerplate code to display instance1 cpu usage

--- src/main/java/eu/ladeira/grafana/Main.java
+++ src/main/java/eu/ladeira/grafana/Main.java
@@ -1,36 +1,38 @@
 package eu.ladeira.grafana;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.grafana.foundation.dashboard.Dashboard;
 import com.grafana.foundation.common.Constants;
+import com.grafana.foundation.dashboard.Dashboard;
 import com.grafana.foundation.dashboard.DashboardBuilder;
 import com.grafana.foundation.dashboard.DashboardDashboardTimeBuilder;
 import com.grafana.foundation.dashboard.RowBuilder;
 import com.grafana.foundation.prometheus.DataqueryBuilder;
 import com.grafana.foundation.timeseries.PanelBuilder;
 
-
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) throws JsonProcessingException {
-        Dashboard dashboard = new DashboardBuilder("Sample Dashboard").
-                uid("generated-from-java").
-                tags(List.of("generated", "from", "java")).
-                refresh("1m").
-                time(new DashboardDashboardTimeBuilder().
-                        from("now-30m").
-                        to("now")
-                ).
-                timezone(Constants.TimeZoneBrowser).
-                withRow(new RowBuilder("Overview")).
-                withPanel(new PanelBuilder().
-                        title("Network Received").
-                        unit("bps").
-                        min(0.0).
-                        withTarget(new DataqueryBuilder().
-                                expr("rate(node_network_receive_bytes_total{job=\"integrations/raspberrypi-node\", device!=\"lo\"}[$__rate_interval]) * 8").
-                                legendFormat("{{ device }}")
+        Dashboard dashboard = new DashboardBuilder("Instance1 - System Metrics").
+                uid("instance1-system-metrics")
+                .refresh("1m")
+                .time(new DashboardDashboardTimeBuilder()
+                        .from("now-30m")
+                        .to("now")
+                )
+                .timezone(Constants.TimeZoneBrowser)
+                .withRow(new RowBuilder("Overview"))
+                .withPanel(new PanelBuilder()
+                        .title("Instance1 - CPU Usage (avg, %)")
+                        .unit("%")
+                        .min(0.0)
+                        .withTarget(new DataqueryBuilder()
+                                .expr("cpu_usage")
+                        )
+                ).withPanel(new PanelBuilder()
+                        .title("Instance1 - CPU Usage (p95, %)")
+                        .unit("%")
+                        .min(0.0)
+                        .withTarget(new DataqueryBuilder()
+                                .expr("quantile_over_time(0.95, cpu_usage[5m])")
                         )
                 ).build();
 
